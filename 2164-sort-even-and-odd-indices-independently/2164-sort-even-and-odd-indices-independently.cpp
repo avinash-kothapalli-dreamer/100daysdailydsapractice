304edfcb3acfae9185bfc50fa404fa39526e47f0
avinash-kothapalli-dreamer <108252893+avinash-kothapalli-dreamer@users.noreply.github.com>
2023-08-17 21:40:47 +0530

    Time: 10 ms (31.71%), Space: 12.3 MB (7.51%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.cpp
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
+    static bool comp(int &a,int &b){
+        return a>b;
+    }
     vector<int> sortEvenOdd(vector<int>& nums) {
-        vector<int> odd, even;
-        for(int i = 0; i < nums.size(); i++) {
-            if(i & 1) {
-                odd.push_back(nums[i]);
-            } else {
-                even.push_back(nums[i]);
-            }
+        vector<int>even,odd;
+        for(int i=0;i<nums.size();i++){
+            if(i%2 == 0)even.push_back(nums[i]);
+            else odd.push_back(nums[i]);
         }
-        sort(odd.begin(), odd.end(), greater<int>());
-        sort(even.begin(), even.end());
-        for(int i = 0; i < nums.size(); i++) {
-            if(i & 1) {
-                nums[i] = odd[0];
-                odd.erase(odd.begin());
-            } else {
-                nums[i] = even[0];
-                even.erase(even.begin());
-            }
+        
+        sort(odd.begin(),odd.end(),comp);
+        sort(even.begin(),even.end());
+        
+        nums.clear();
+        
+        int k = even.size();
+        int j = odd.size();
+        int i=0;
+        int n = 0;
+        while(i<k && n<j){
+            nums.push_back(even[i]);
+            nums.push_back(odd[n]);
+            i++;
+            n++;
         }
+        
+        while(i<k) {
+            nums.push_back(even[i]);
+            i++;
+        }
+        while(n<j) {
+            nums.push_back(odd[n]);
+            n++;
+        }
+        
         return nums;
     }
 };
