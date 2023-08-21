25e993d7fe31a10ad0cab25e259ce0f375c47fc5
avinash-kothapalli-dreamer <108252893+avinash-kothapalli-dreamer@users.noreply.github.com>
2023-08-21 22:13:14 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (81.46%) - LeetHub

--- 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
+++ 0600-non-negative-integers-without-consecutive-ones/0600-non-negative-integers-without-consecutive-ones.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     int findIntegers(int n) {
-        int arr[32] ;
-        arr[0] = 1;
-        arr[1] = 2;
+       int f[32]={0};
+        f[0]=1;
+        f[1]= 2;
         
-        for(int i = 2;i<32;i++) arr[i] = arr[i-1]+arr[i-2];
+        for(int i=2;i<32;i++) f[i] = f[i-1]+f[i-2];
         
-        int i = 30,prev_bit=0,ans=0;
+        int i=31;
+        int ans= 0;
+        int prev_bit = 0;
         
         while(i>=0){
-           if(n & (1 << i)){
-               ans+=arr[i];
-               if(prev_bit == 1){
-                   ans--;
-                   break;
-               }
-               
-               prev_bit = 1;
-           }else{
-               prev_bit = 0;
-           } 
+            if(n & (1<<i)){
+                ans+=f[i];
+                if(prev_bit==1){
+                    ans--;
+                    break;
+                }
+                prev_bit =1;
+            }else{
+                prev_bit=0;
+            }
+            
             i--;
         }
         
-       return ans+1; 
-        
+        return ans+1;
     }
 };
