aece0dfd7344e912f3d9d8be9d6db77d3633d148
avinash-kothapalli-dreamer <108252893+avinash-kothapalli-dreamer@users.noreply.github.com>
2023-09-04 20:45:28 +0530

    Time: 285 ms (64.63%), Space: 117.8 MB (46.83%) - LeetHub

--- 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
+++ 1483-kth-ancestor-of-a-tree-node/1483-kth-ancestor-of-a-tree-node.cpp
@@ -20,8 +20,8 @@ public:
     }
     
     int getKthAncestor(int node, int k) {
-         for (int i = 0; i < 20; i++) {
-            if ((k >> i) & 1) {
+         for (int i = 0; (1<<i) <=k; i++) {
+            if ((1<<i)&k) {
                 node = v[node][i];
                 if (node == -1) return -1;
             }
