9dc2b5a42b007dfeacba1fa48bbee5a1e6d63283
avinash-kothapalli-dreamer <108252893+avinash-kothapalli-dreamer@users.noreply.github.com>
2023-09-17 11:55:29 +0530

    Time: 78 ms (18.18%), Space: 55.4 MB (18.18%) - LeetHub

--- 2861-maximum-number-of-alloys/2861-maximum-number-of-alloys.cpp
+++ 2861-maximum-number-of-alloys/2861-maximum-number-of-alloys.cpp
@@ -1,7 +1,7 @@
 using ll = long long;
 class Solution {
 public:
-    bool func(ll mid, int budget, vector<vector<int>>& composition, vector<int>& stock, vector<int>& cost)
+ bool func(ll mid, int budget, vector<vector<int>>& composition, vector<int>& stock, vector<int>& cost)
     {
         ll minCost = INT_MAX;
         for (int i = 0; i < composition.size(); ++i)
@@ -23,17 +23,17 @@ public:
         return (minCost <= budget);
     }
     int maxNumberOfAlloys(int n, int k, int budget, vector<vector<int>>& composition, vector<int>& stock, vector<int>& cost) {
-        ll low = 0, high = 1e9, ans = 0;
-        while (low <= high)
-        {
-            ll mid = low + (high - low) / 2;
-            if (func(mid, budget, composition, stock, cost))
-            {
+        int low = 0,high = 1e9;
+        int ans = 0;
+        while(low<=high){
+            ll mid = (low+high)/2;
+            
+            if(func(mid,budget,composition,stock,cost)){
                 ans = mid;
-                low = mid + 1;
+                low = mid+1;
+            }else{
+                high = mid-1;
             }
-            else
-                high = mid - 1;
         }
         return ans;
     }
